7d9a642ab9c1a01f09e5d17cbf891cc5b20a7467
dasom <dasom2334@gmail.com>
2022-06-17 02:41:19 +0900

    works do what ben want

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import {MikroORM } from "@mikro-orm/core";
+import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants"
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
-
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig);
@@ -11,8 +14,24 @@ const main = async () => {
     // await orm.em.persistAndFlush(post);
     // await orm.em.nativeInsert(Post, {title: 'my second post'});
 
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
+    // const posts = await orm.em.find(Post, {});
+    // console.log(posts);
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
+    apolloServer.applyMiddleware({ app });
+
+    // app.get('/', (_, res) => {
+    //     res.send('hello');
+    // });
+    app.listen(4000, () => {
+        console.log('server start port 4000');
+    });
 };
 main().catch(err => {
     console.error(err);
