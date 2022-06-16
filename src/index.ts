ac511378a6422c181d6041251949cd80d8ecd39e
dasom <dasom2334@gmail.com>
2022-06-16 18:48:11 +0900

    MicroORM Setup

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import {MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants"
+import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
+
+
+const main = async () => {
+    const orm = await MikroORM.init(mikroConfig);
+
+    const post = orm.em.fork({}).create(Post, {title: 'my first post'});
+    await orm.em.persistAndFlush(post);
+    await orm.em.nativeInsert(Post, {title: 'my second post'});
+};
+main().catch(err => {
+    console.error(err);
+});
+// console.log('hiwefwefhwefwefei');
+//# sourceMappingURL=index.js.map
