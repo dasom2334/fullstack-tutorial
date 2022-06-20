fd03cd98c9d1532645b827f3ca94751c05ffdd77
dasom <dasom2334@gmail.com>
2022-06-21 00:14:28 +0900

    RedisStore is not assignable type RedisClientType fixed https://github.com/tj/connect-redis/issues/300 @types/connect-redis의 문제점

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,91 @@
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants"
+import { __prod__ } from "./constants";
 // import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/Post";
-import 'reflect-metadata';
+import "reflect-metadata";
 import { UserResolver } from "./resolvers/User";
+import session from 'express-session';
+import { createClient } from "redis";
+import connectRedis from 'connect-redis';
 
 const main = async () => {
-    const orm = await MikroORM.init(mikroConfig);
-    await orm.getMigrator().up();
-    // const post = orm.em.fork({}).create(Post, {title: 'my first post'});
-    // await orm.em.persistAndFlush(post);
-    // await orm.em.nativeInsert(Post, {title: 'my second post'});
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
+  // const post = orm.em.fork({}).create(Post, {title: 'my first post'});
+  // await orm.em.persistAndFlush(post);
+  // await orm.em.nativeInsert(Post, {title: 'my second post'});
 
-    // const posts = await orm.em.find(Post, {});
-    // console.log(posts);
-    const app = express();
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
+  const app = express();
 
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver, UserResolver],
-            validate: false
+  let RedisStore = connectRedis(session);
+  // const { createClient } = require("redis")
+
+  // const redisClient = createClient({ 
+  //   legacyMode: true,
+  //   url: '127.0.0.1:6379',
+  // });
+  // await redisClient.connect().catch(console.error);
+
+  let redisClient = createClient({ legacyMode: true })
+  redisClient.connect().catch(console.error)
+  // const redisClient = createClient(); 
+
+  app.use(
+    session({
+      store: new RedisStore({
+         client: redisClient as any,
+         disableTouch: true,
+         disableTTL: true,
         }),
-        context: () => ({ em: orm.em })
-    });
-    await apolloServer.start();
+      saveUninitialized: false,
+      secret: "keyboard cat",
+      resave: false,
+    })
+  )
+//   const RedisStore = connectRedis(session);
+//   const redisClient = createClient({ legacyMode: true });
+//   redisClient.connect().catch(console.error);
 
-    apolloServer.applyMiddleware({ app });
+//   app.use(
+//     session({
+//       store: new RedisStore({ 
+//         // client: redisClient,
+//             client: redisClient,
+//             disableTouch: true,
+//         }),
+//       saveUninitialized: false,
+//       secret: "IMissYouZesu",
+//       resave: false,
+//     })
+//   );
 
-    // app.get('/', (_, res) => {
-    //     res.send('hello');
-    // });
-    app.listen(4000, () => {
-        console.log('server start port 4000');
-    });
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  // app.get('/', (_, res) => {
+  //     res.send('hello');
+  // });
+  app.listen(4000, () => {
+    console.log("server start port 4000");
+  });
 };
-main().catch(err => {
-    console.error(err);
+main().catch((err) => {
+  console.error(err);
 });
 // console.log('hiwefwefhwefwefei');
 //# sourceMappingURL=index.js.map
